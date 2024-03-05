b1a0be4afba0f32a6a0ada9f256d32c11cb68925
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-03-05 16:53:17 +0300

    Ex-1 Control Flow

---
+++ LAB-1-Control-Flow-Statments-.dart
@@ -0,0 +1,29 @@
+void main() {
+  var integer = '7';
+
+  switch (integer) {
+    case '1':
+      print('Monday');
+      break;
+    case '2':
+      print('Tuesday');
+      break;
+    case '3':
+      print('Wednsday');
+      break;
+    case '4':
+      print('Thursday');
+      break;
+    case '5':
+      print('Friday');
+      break;
+    case '6':
+      print('Saturday');
+      break;
+    case '7':
+      print('Sunday');
+      break;
+    default:
+      print('No days of the week which corresponds to the number!');
+  }
+}

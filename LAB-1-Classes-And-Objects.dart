6f9200150f779878ba60c16b756bc5ffb43580ce
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-03-05 16:52:41 +0300

    Ex-1 Classes and Objects

---
+++ LAB-1-Classes-And-Objects.dart
@@ -0,0 +1,16 @@
+class Car {
+  String brand;
+  String model;
+  int year;
+
+  Car(this.brand, this.model, this.year);
+}
+
+void main() {
+  Car exampleCar = Car('Suzuki', 'Swift', 2022);
+
+  print("Brand: ${exampleCar.brand}");
+  print("Model: ${exampleCar.model}");
+  print("Year: ${exampleCar.year}");
+
+}
